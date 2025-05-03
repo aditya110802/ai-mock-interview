4d30087a11157d87ed51422d9132e77b54a913e0
aditya110802 <aditya110802verma@gmail.com>
2025-05-03 20:43:34 +0530

    changed status to ACTIVE so that error should stop

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,52 +1,51 @@
 'use server';
 
 import { db, auth} from "@/firebase/admin";
-import { CollectionReference } from "firebase/firestore";
 import { cookies } from "next/headers";
 
 const SESSION_DURATION=60*60*24*7;
 
 export async function signUp(params: SignUpParams) {
-    const { uid, name, email } = params;
-  
-    try {
-      // check if user exists in db
-      const userRecord = await db.collection("users").doc(uid).get();
-      if (userRecord.exists)
-        return {
-          success: false,
-          message: "User already exists. Please sign in.",
-        };
-  
-      // save user to db
-      await db.collection("users").doc(uid).set({
-        name,
-        email,
-        // profileURL,
-        // resumeURL,
-      });
-  
-      return {
-        success: true,
-        message: "Account created successfully. Please sign in.",
-      };
-    } catch (error: any) {
-      console.error("Error creating user:", error);
-  
-      // Handle Firebase specific errors
-      if (error.code === "auth/email-already-exists") {
-        return {
-          success: false,
-          message: "This email is already in use",
-        };
-      }
-  
+  const { uid, name, email } = params;
+
+  try {
+    // check if user exists in db
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
       return {
         success: false,
-        message: "Failed to create account. Please try again.",
+        message: "User already exists. Please sign in.",
+      };
+
+    // save user to db
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+
+    // Handle Firebase specific errors
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
       };
     }
+
+    return {
+      success: false,
+      message: "Failed to create account. Please try again.",
+    };
   }
+}
 
   export async function signIn(params: SignInParams) {
     const { email, idToken } = params;
@@ -88,31 +87,34 @@ export async function signUp(params: SignUpParams) {
     });
   }
 
-  export async function getCurrentUser(): Promise<User |null> {
+  export async function getCurrentUser(): Promise<User | null> {
     const cookieStore = await cookies();
-
-    const sessionCookie=cookieStore.get('session')?.value;
-    if(!sessionCookie) return null;
-    try{
-        const decodedClaims= await auth.verifySessionCookie(sessionCookie, true);
-
-        const userRecord = await db.
-        collection('users')
+  
+    const sessionCookie = cookieStore.get("session")?.value;
+    if (!sessionCookie) return null;
+  
+    try {
+      const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+  
+      // get user info from db
+      const userRecord = await db
+        .collection("users")
         .doc(decodedClaims.uid)
         .get();
-
-        if(!userRecord.exists) return null;
-
-        return{
-            ...userRecord.data(),
-            id: userRecord.id,
-          } as User;
-
-    }catch(e){
-        console.log(e);
-        return null;
+      if (!userRecord.exists) return null;
+  
+      return {
+        ...userRecord.data(),
+        id: userRecord.id,
+      } as User;
+    } catch (error) {
+      console.log(error);
+  
+      // Invalid or expired session
+      return null;
     }
   }
+
   export async function isAuthenticated() {
     const user = await getCurrentUser();
     return !!user;
